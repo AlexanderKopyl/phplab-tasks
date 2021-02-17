7b677a8acf75327189b868ebfdc53887ec98ca83
Kopul@ua.fm <Kopul@ua.fm>
2021-02-17 14:05:02 +0200

    Work with migrations . Add all needed function

--- src/db/migrations.php
+++ src/db/migrations.php
@@ -35,5 +35,31 @@ SQL;
 $pdo->exec($sql);
 
 // TODO states
-
+// cities
+$sql = <<<'SQL'
+CREATE TABLE `state` (
+	`id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(50) NOT NULL COLLATE 'utf8_general_ci',
+	PRIMARY KEY (`id`)
+);
+SQL;
+$pdo->exec($sql);
 // TODO airports
+// cities
+$sql = <<<'SQL'
+CREATE TABLE `airports` (
+	`id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(50) NOT NULL COLLATE 'utf8_general_ci',
+	`state_id` INT(10) UNSIGNED NOT NULL COLLATE 'utf8_general_ci',
+	`city_id` INT(10) UNSIGNED NOT NULL COLLATE 'utf8_general_ci',
+	`code` VARCHAR(5) NOT NULL COLLATE 'utf8_general_ci',
+	`address` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+	`timezone` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+	PRIMARY KEY (`id`)
+);
+
+ALTER TABLE `airports` ADD  FOREIGN KEY (`state_id`) REFERENCES `state`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+ALTER TABLE `airports` ADD  FOREIGN KEY (`city_id`) REFERENCES `cities`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+SQL;
+$pdo->exec($sql);
